fb0c996d3403ccfb189c18f4292874b76138d966
Aryan Gandhi <aryandgandhi@gmail.com>
2023-03-17 20:54:22 -0500

    new

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,28 @@ import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('')
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -27,12 +49,15 @@ const Home = () => {
         <div className="prompt-container">
           <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
           <div className="prompt-buttons">
-              <a className="generate-button" onClick={null}>
-                <div className="generate">
-                  <p>Generate</p>
-      </div>
-    </a>
-  </div>
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>
         </div>
       </div>
       <div className="badge-container grow">
